c80b843842bb3b962cd14c101f071ef514c7a5fb
MousaZa <mous.zeydan@gmail.com>
2026-03-20 15:41:06 +0300

    Added tasks in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+/*
+ * Task to do before next week:
+ * 1. Make Seperate Bow, Wand, Sword classes and make them children of the Weapon class
+ * 2. Develop a mechanism to recover when the Weapon's durability becomes 0
+ */
+
 int main(){
 
     Game game = Game();
